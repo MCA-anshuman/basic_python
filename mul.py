3417cd08520de92a9e304b26e537fbe7b96f1116
MCA-anshuman <mr.shivansh0645@gmail.com>
2025-12-22 22:19:03 +0530

    Add files via upload

---
+++ mul.py
@@ -0,0 +1,8 @@
+no=int(float(input("Enter first no:-")))
+no1=int(float(input("Enter second no:-")))
+r=float=s=no*no1
+print("multiply no & no1:-",r)  #no*no1)
+print(type(no))
+print(type(no1))
+print(type(s))
+print(type(r))
