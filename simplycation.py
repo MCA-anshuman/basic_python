6da20049d33c9b19a9af6d3eb30c1a69b6ce556f
MCA-anshuman <mr.shivansh0645@gmail.com>
2025-12-22 22:13:10 +0530

    Add files via upload

---
+++ simplycation.py
@@ -0,0 +1,14 @@
+#arithmetic operators 
+a=5
+b=2                     #single_Line_Comment
+print("sum :",a+b)            #Addition
+sub=a-b
+print("subctract :",sub)     #subctraction
+print("multiple :",a*b)      #multiplication
+diff=a/b  
+print("divide :",diff)        #divition
+print("remender is :",a%b)    #Remender  
+"""
+ multi-Line
+ coment
+"""
