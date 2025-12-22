32b06872ee405f3380c223b148a7d3549fdd2467
MCA-anshuman <mr.shivansh0645@gmail.com>
2025-12-22 22:22:39 +0530

    Add files via upload

---
+++ greaterthan.py
@@ -0,0 +1,6 @@
+#WAP to input 2 int number, a and b.
+# print true if a is greater than or equale to b, print false.
+ 
+A=int(input("Enter the first no:"))
+B=int(input("Ente the second no:"))
+print(A >= B)
