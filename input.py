7fe671be7059dd61333d735ce3ccf6622fcd6fe3
MCA-anshuman <mr.shivansh0645@gmail.com>
2025-12-22 22:16:46 +0530

    Add files via upload

---
+++ input.py
@@ -0,0 +1,11 @@
+name = input("enter your name:")
+ #print("welcome ",val)   # "23", "12.99"
+
+int("1")
+age = int(input("enter your age:"))
+ #print( type(val), "age :",val) 
+course =input("enter your course:") 
+
+print("welcome ",name)
+print( "your age:",age)
+print("your course:",course)
