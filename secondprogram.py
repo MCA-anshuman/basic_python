b202955129a083f4ca95f4ee7cee41c2f06a72ab
MCA-anshuman <mr.shivansh0645@gmail.com>
2025-12-22 22:13:52 +0530

    Add files via upload

---
+++ secondprogram.py
@@ -0,0 +1,37 @@
+  #arithmetic operator
+""" +
+   -
+   *
+   /"""
+
+  #relational operators
+a = 10
+b =18
+
+print(a == b)
+print(a !=b)
+print(a >=b)
+print(a >b)
+
+   #assignment operators
+num=10
+#num= num+10     #10+10=>20
+"""num += 10        #short_cort
+num -= 10
+num *= 5
+num /= 2
+num %=5"""
+num **= 5
+print("num :",num)
+
+    #logical operators
+c = 60
+d = 40 
+print(not False)
+print(not (c > d))
+
+val1 = True
+val2 = False
+print("AND operator:",val1 and val2)
+
+print("OR operator:",(c == d)  or (c > d))
