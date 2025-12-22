704ee14c357fc05d5b3f01d446307d1c56c2f262
MCA-anshuman <mr.shivansh0645@gmail.com>
2025-12-22 22:15:52 +0530

    Add files via upload

---
+++ type_conversion.py
@@ -0,0 +1,18 @@
+#type conversion
+a = 2              #automatic conversion
+b = 5.25
+            
+sum = a+b  # 2.0 + 5.25
+print("sum:",sum)
+
+#type casating
+c = int('10')     #manually conversion
+#c = int('anshuman')   #only vale is convert string to intiger or float,so it is not work..! 
+d = 2.35 
+print('sum:',c+d)
+
+
+r = 1.25
+s = str(r)
+
+print(type(s), s)
