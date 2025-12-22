91f58128e2b9968b0e1d2ad6358da9157c524189
MCA-anshuman <mr.shivansh0645@gmail.com>
2025-12-22 22:09:08 +0530

    Add files via upload

---
+++ firstprogram.py
@@ -0,0 +1,21 @@
+# variables & Data Types 
+print("I am anshuman raj")
+print("My roll is 15")
+print(35+35)
+course="BCA"
+roll=226268
+fee=60.500
+print(course)   
+print("I am a student of :",course)
+print(type(fee))
+name1="as"
+name2='as'
+name3='''as'''
+print(name1)
+print(name2)
+print(name3)
+a=None
+old=False
+print(type(old))
+print(type(a))
+print(type(roll))
