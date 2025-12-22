efe73d936f99fab9b40900cdd08c9747fd3c92d8
MCA-anshuman <mr.shivansh0645@gmail.com>
2025-12-22 22:23:15 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,26 @@
+# string is data type that stores a sequence of characters.
+
+str = "this is a string"
+str1 = 'Dr RMLS Collage'
+str2 = '''Bacholar Of Computer Application'''
+   #why strings:- ex:("this is Enginnering's Collage")
+
+print(str )
+print(str1)
+print(str2)
+
+#is called escape sequence characters ()
+
+str3 = "this is a string. \nwe are creating it in python,"    #next Line
+str4 = "this is a string.\twe are creating it in python,"      #Tab
+print(str3)
+print(str4)
+
+# Concatenation
+print(str1+" "+str2)
+
+# length of str
+print(len(str))
+
+final_str = str1+" "+str2
+print(len(final_str))
