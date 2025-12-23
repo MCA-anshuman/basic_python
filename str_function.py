d813244f3c7a775d71e38d9d90bab43535733234
MCA-anshuman <mr.shivansh0645@gmail.com>
2025-12-23 23:48:29 +0530

    Add files via upload

---
+++ str_function.py
@@ -0,0 +1,7 @@
+str = "I am a Coder."
+
+print(str.endswith("er."))  #returns true if string ends with substr.
+print(str.capitalize())     #capital 1st char.
+print(str.replace("I am", "you are"))  #replace all occurences of old.
+print(str.find("a"))            #return 1st index occurre.
+print(str.count("m"))            #count the occurence of substr. 
