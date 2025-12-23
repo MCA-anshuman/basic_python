6d48e9050fda8d030d5378993d908d5160670b5c
MCA-anshuman <mr.shivansh0645@gmail.com>
2025-12-23 23:57:08 +0530

    Add files via upload

---
+++ list-basic.py
@@ -0,0 +1,15 @@
+#list:-
+      # A built-in data type that stores set of values
+      #it can store elements of different type (integer,float,string,etc.)
+
+'''marks = [94.4,87.5,79,66,45.1]  #index      #muteble
+print(type(marks)) 
+print(marks)
+print(marks[0])
+print(marks[1])
+print(len(marks))'''
+
+student = ["kanha",108,"delhi" ]
+student[0] = "Radha"          #allowed in python    or string is not allowed.
+print(student)
+print(len(student))            #returns length
