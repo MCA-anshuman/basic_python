793544a5036bee12aead1742277997e11a41a41c
MCA-anshuman <mr.shivansh0645@gmail.com>
2025-12-23 23:52:43 +0530

    Add files via upload

---
+++ elseif_condition_trafice.py
@@ -0,0 +1,9 @@
+#WAP  cheque trafice light rule, through elseif condition.
+
+light = "green"
+if(light == "red"):
+    print("Stop")
+elif(light == "green"):
+    print("Go")
+elif(light == "yellow"):
+    print("look")
