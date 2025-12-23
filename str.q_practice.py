10b70ade6ee008d197151435f387f4c218ae019b
MCA-anshuman <mr.shivansh0645@gmail.com>
2025-12-23 23:49:00 +0530

    Add files via upload

---
+++ str.q_practice.py
@@ -0,0 +1,8 @@
+#WAP to inpute user's name & print its length.
+
+name = input("Enter user's name:")
+print("length of your name is:", len(name))
+
+#WAP to find the occurrence of '$' in a string.
+str = "hii, $Iam the $ symbol $99.99"
+print(str.count("$"))
