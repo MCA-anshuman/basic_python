4123175af00e148b944ae6f2ad047f157774bfc4
MCA-anshuman <mr.shivansh0645@gmail.com>
2025-12-23 23:47:23 +0530

    Add files via upload

---
+++ indexing-Slicing.py
@@ -0,0 +1,22 @@
+ #index
+index = "ENGINEERING"
+
+print(index[0],[1],[2]) 
+print(len(index))
+
+
+ # Slicing            
+              #substring 
+"""
+  Accessing part of a String
+  str[starting_idx : ending_idx]  # ending idx is not included
+
+"""
+str = "Placement_"
+print(str[5:10])
+print(len(str))
+
+'''negatgiv Slicing'''
+str1 = "Collage"      # -7,-6,-5,-4,-3,-2,-1
+print(str1[-3:])
+
