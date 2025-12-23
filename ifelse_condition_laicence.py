8b56bd479afe9613948caa152ef8cf549a84bc91
MCA-anshuman <mr.shivansh0645@gmail.com>
2025-12-23 23:53:31 +0530

    Add files via upload

---
+++ ifelse_condition_laicence.py
@@ -0,0 +1,6 @@
+#WAP to input age & cheque elligible for license, through ifelse condition.
+age = int(input("Enter age for license="))
+if(age >= 18):
+    print("can apply license")
+else:
+    print("Sorry..! cheque your age/only 18+ previlage apply.")
