01ba2c026e59f9d5693a576657fa405e6eed90c7
MCA-anshuman <mr.shivansh0645@gmail.com>
2025-12-28 00:33:09 +0530

    Add files via upload

---
+++ list-Slicing.py
@@ -0,0 +1,10 @@
+#Similar to string Slicing.
+  #list_name[starting_idx : ending_idx]   'ending_idx is not included'
+  
+marks = [90,16,78,95,33]
+print(marks[1:4])
+print(marks[:4])
+print(marks[3:])
+print(marks[-3:-1])
+print(marks[-6: ])
+print(marks[ :-1])
