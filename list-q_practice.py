7b0caa215671efd2a2854a9ed278f717e8bbc144
MCA-anshuman <mr.shivansh0645@gmail.com>
2025-12-28 00:35:36 +0530

    Add files via upload

---
+++ list-q_practice.py
@@ -0,0 +1,8 @@
+#WAP to ask the user to enter names of thier 3 favorite movies & store them in a list.
+
+a = input("Enter names of thier 3 favorite movies= ,\n")
+b = input()
+c = input()
+list = [a,b,c]
+print(list)
+print(type(list))
